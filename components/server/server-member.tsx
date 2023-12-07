b9e24f12aa5886efdd9a7334fabf368d7b05c857
hoamx <hoamx.work@gmail.com>
2023-12-07 23:30:57 +0700

    feat: handle conversation setup

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { cn } from '@/lib/utils';
 import { Member, MemberRole, Profile, Server } from '@prisma/client';
 import { ShieldAlert, ShieldCheck } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
+
+import { cn } from '@/lib/utils';
 import { UserAvatar } from '@/components/user-avatar';
 
 interface ServerMemberProps {
@@ -26,11 +27,12 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
   const icon = roleIconMap[member.role];
 
   const onClick = () => {
-    router.push(`/servers/${params?.serverId}/conversations/${server?.id}`)
-  }
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+  };
+
   return (
     <button
-    onClick={onClick}
+      onClick={onClick}
       className={cn(
         'group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1',
         params?.memberId === member.id && 'bg-zinc-700/20 dark:bg-zinc-700'
