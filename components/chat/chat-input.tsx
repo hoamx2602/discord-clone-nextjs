89486803702fa30387711d8727ef7dcaed18e2b8
hoamx <hoamx.work@gmail.com>
2023-12-08 23:12:44 +0700

    feat: handle message attachment

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -7,8 +7,8 @@ import * as z from 'zod';
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Plus, Smile } from 'lucide-react';
-import axios from "axios";
-import qs from "query-string";
+import axios from 'axios';
+import qs from 'query-string';
 
 interface ChatInputProps {
   apiUrl: string;
@@ -50,38 +50,42 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
     }
   };
 
-  return <Form {...form}>
-  <form onSubmit={form.handleSubmit(onSubmit)}>
-    <FormField
-      control={form.control}
-      name="content"
-      render={({ field }) => (
-        <FormItem>
-          <FormControl>
-            <div className="relative p-4 pb-6">
-              <button
-                type="button"
-                onClick={() => {}}
-                className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
-              >
-                <Plus className="text-white dark:text-[#313338]" />
-              </button>
-              <Input
-                disabled={isLoading}
-                className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
-                placeholder={`Message ${type === "conversation" ? name : "#" + name}`}
-                {...field}
-              />
-              <div className="absolute top-7 right-8">
-                <Smile
-                //   onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
-                />
-              </div>
-            </div>
-          </FormControl>
-        </FormItem>
-      )}
-    />
-  </form>
-</Form>;
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <div className="relative p-4 pb-6">
+                  <button
+                    type="button"
+                    onClick={() => onOpen('messageFile', { apiUrl, query })}
+                    className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
+                  >
+                    <Plus className="text-white dark:text-[#313338]" />
+                  </button>
+                  <Input
+                    disabled={isLoading}
+                    className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                    placeholder={`Message ${
+                      type === 'conversation' ? name : '#' + name
+                    }`}
+                    {...field}
+                  />
+                  <div className="absolute top-7 right-8">
+                    <Smile
+                    //   onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                    />
+                  </div>
+                </div>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
 };
