dd1f59936d9e57d6267525a12eee1998af71f19e
hoamx <hoamx.work@gmail.com>
2023-12-09 07:21:52 +0700

    feat: add chat messages component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import './globals.css';
+import { QueryProvider } from '@/components/providers/query-provider';
 
 const font = Open_Sans({ subsets: ['latin'] });
 
@@ -31,7 +32,7 @@ export default function RootLayout({
           >
             <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>{children}</QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
